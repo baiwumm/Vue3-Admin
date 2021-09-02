862ce891515e8f6e87c389d96df49f4a813c674b
xiemingwei <843348394@qq.com>
2021-09-02 14:39:53 +0800

    feat: 添加二维码模块和添加水印

--- Xmw_web/src/layouts/default/content/index.vue
+++ Xmw_web/src/layouts/default/content/index.vue
@@ -1,51 +1,58 @@
 <template>
-  <div :class="[prefixCls, getLayoutContentMode]" v-loading="getOpenPageLoading && getPageLoading">
-    <PageLayout />
-  </div>
+    <div
+        :class="[prefixCls, getLayoutContentMode]"
+        v-loading="getOpenPageLoading && getPageLoading"
+    >
+        <PageLayout />
+    </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import PageLayout from '/@/layouts/page/index.vue';
-  import { useDesign } from '/@/hooks/web/useDesign';
-  import { useRootSetting } from '/@/hooks/setting/useRootSetting';
-  import { useTransitionSetting } from '/@/hooks/setting/useTransitionSetting';
-  import { useContentViewHeight } from './useContentViewHeight';
+import { defineComponent, onMounted } from 'vue';
+import PageLayout from '/@/layouts/page/index.vue';
+import { useDesign } from '/@/hooks/web/useDesign';
+import { useRootSetting } from '/@/hooks/setting/useRootSetting';
+import { useTransitionSetting } from '/@/hooks/setting/useTransitionSetting';
+import { useContentViewHeight } from './useContentViewHeight';
+import { useWatermark } from '/@/hooks/web/useWatermark';
 
-  export default defineComponent({
+export default defineComponent({
     name: 'LayoutContent',
     components: { PageLayout },
     setup() {
-      const { prefixCls } = useDesign('layout-content');
-      const { getOpenPageLoading } = useTransitionSetting();
-      const { getLayoutContentMode, getPageLoading } = useRootSetting();
-
-      useContentViewHeight();
-      return {
-        prefixCls,
-        getOpenPageLoading,
-        getLayoutContentMode,
-        getPageLoading,
-      };
+        const { prefixCls } = useDesign('layout-content');
+        const { getOpenPageLoading } = useTransitionSetting();
+        const { getLayoutContentMode, getPageLoading } = useRootSetting();
+        const { setWatermark } = useWatermark();
+        useContentViewHeight();
+        onMounted(() => {
+            setWatermark('Xmw Admin')
+        })
+        return {
+            prefixCls,
+            getOpenPageLoading,
+            getLayoutContentMode,
+            getPageLoading,
+        };
     },
-  });
+});
 </script>
 <style lang="less">
-  @prefix-cls: ~'@{namespace}-layout-content';
+@prefix-cls: ~"@{namespace}-layout-content";
 
-  .@{prefix-cls} {
+.@{prefix-cls} {
     position: relative;
     flex: 1 1 auto;
     min-height: 0;
 
     &.fixed {
-      width: 1200px;
-      margin: 0 auto;
+        width: 1200px;
+        margin: 0 auto;
     }
 
     &-loading {
-      position: absolute;
-      top: 200px;
-      z-index: @page-loading-z-index;
+        position: absolute;
+        top: 200px;
+        z-index: @page-loading-z-index;
     }
-  }
+}
 </style>
---
+++ Xmw_web/src/views/features/qrcode/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <PageWrapper :title="t('router.features.qrcode.title')">
+        <Row :gutter="20">
+            <Col
+                :xs="24"
+                :sm="12"
+                :md="8"
+                :lg="6"
+                :xl="6"
+                v-for="(qr,index) in qrCodeList"
+                :key="index"
+                style="margin-bottom: 20px;"
+            >
+                <CollapseContainer :title="qr.title" class="text-center">
+                    <QrCode
+                        :value="qr.url"
+                        :options="qr.options"
+                        :tag="qr.tag"
+                        :logo="qr.logo"
+                        :width="qr.width"
+                    />
+                </CollapseContainer>
+            </Col>
+            <Col :xs="24" :sm="12" :md="8" :lg="6" :xl="6" style="margin-bottom: 20px;">
+                <CollapseContainer :title="t('router.features.qrcode.title8')" class="text-center">
+                    <QrCode
+                        :value="qrCodeUrl"
+                        :width="260"
+                        :options="{ margin: 5 }"
+                        ref="qrDiyRef"
+                        :logo="LogoImg"
+                        @done="onQrcodeDone"
+                    />
+                    <a-button class="mb-2" type="primary" @click="downloadDiy">下载</a-button>
+                </CollapseContainer>
+            </Col>
+        </Row>
+    </PageWrapper>
+</template>
+<script lang="ts" setup>
+import { ref, unref } from 'vue';
+import { QrCode } from '/@/components/Qrcode/index';
+import LogoImg from '/@/assets/images/logo.png';
+import { CollapseContainer } from '/@/components/Container/index';
+import { PageWrapper } from '/@/components/Page';
+import { Row, Col } from 'ant-design-vue';
+import { useI18n } from '/@/hooks/web/useI18n';
+export interface QrCodeActionType {
+    download: (fileName?: string) => void;
+}
+export interface QrCodeResult {
+    title: string;
+    url: string;
+    tag?: 'canvas' | 'img' | undefined;
+    options?: Object,
+    logo?: string | Object,
+    width?: number | undefined
+}
+const { t } = useI18n();
+const qrCodeUrl = 'https://www.baidu.cn';
+const qrCodeList: QrCodeResult[] = [
+    { title: t('router.features.qrcode.title1'), url: qrCodeUrl },
+    { title: t('router.features.qrcode.title2'), url: qrCodeUrl, tag: 'img' },
+    { title: t('router.features.qrcode.title3'), url: qrCodeUrl, options: { color: { dark: '#55D187' } } },
+    { title: t('router.features.qrcode.title4'), url: qrCodeUrl, logo: LogoImg },
+    { title: t('router.features.qrcode.title5'), url: qrCodeUrl, options: { color: { dark: '#55D187' } }, width: 300, logo: 'https://xmwpro.oss-cn-beijing.aliyuncs.com/vue-admin-xmw-pro/logo.svg' },
+    { title: t('router.features.qrcode.title6'), url: qrCodeUrl, width: 300, logo: { src: LogoImg, logoSize: 0.2, borderSize: 0.05, borderRadius: 50, bgColor: 'gold' } },
+    { title: t('router.features.qrcode.title7'), url: qrCodeUrl, width: 300 },
+]
+
+const qrDiyRef = ref<Nullable<QrCodeActionType>>(null);
+
+function downloadDiy() {
+    const qrEl = unref(qrDiyRef);
+    if (!qrEl) return;
+    qrEl.download('Qrcode');
+}
+
+function onQrcodeDone({ ctx }) {
+    if (ctx instanceof CanvasRenderingContext2D) {
+        // 额外绘制
+        ctx.fillStyle = 'black';
+        ctx.font = '16px "微软雅黑"';
+        ctx.textBaseline = 'bottom';
+        ctx.textAlign = 'center';
+        ctx.fillText(t('router.features.qrcode.fillText'), 125, 250, 200);
+    }
+}
+</script>
+<style lang='less' scoped>
+/deep/ canvas,
+/deep/ img {
+    display: inline-block;
+}
+</style>
