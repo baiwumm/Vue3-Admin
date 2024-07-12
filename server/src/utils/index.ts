47af68f64d8558c38a91199f9d7240dfb23eda9a
baiwumm <baiwumm@foxmail.com>
2024-07-12 15:37:12 +0800

    feat: 添加 removeEmptyChildren 工具类，移除空数组的节点

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,7 +1,6 @@
 import dayjs from 'dayjs';
 
 import { RESPONSE_CODE, RESPONSE_MSG } from '@/enums';
-import type { Response } from '@/types';
 
 /**
  * @description: 统一返回体
@@ -10,4 +9,21 @@ export const responseMessage = <T = any>(
   data,
   msg: string = RESPONSE_MSG.SUCCESS,
   code: number = RESPONSE_CODE.SUCCESS,
-): Response<T> => ({ data, msg, code, timestamp: dayjs().valueOf() });
+): Api.Common.Response<T> => ({ data, msg, code, timestamp: dayjs().valueOf() });
+
+/**
+ * @description: 移除子节点未空数组的节点
+ */
+export const removeEmptyChildren = (tree) => {
+  // 遍历数组中的每个节点
+  tree.forEach((node) => {
+    // 检查当前节点是否有children属性且该数组为空
+    if (node.children && node.children.length === 0) {
+      // 删除当前节点的children属性
+      delete node.children;
+    } else if (node.children) {
+      // 如果children不为空，继续递归清理子节点中的空children
+      removeEmptyChildren(node.children);
+    }
+  });
+};
