fbbd8479ad6ae78bd5150848970a7403884598a0
baiwumm <baiwumm@foxmail.com>
2024-07-12 16:10:20 +0800

    feat: 修改 PrismaModule 为局部导入

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { OrganazationModule } from '@/modules/administrative/organazation/organazation.module'; // 智能行政-组织管理
 import { AuthModule } from '@/modules/auth/auth.module'; // 登录授权
-import { PrismaModule } from '@/modules/prisma/prisma.module';
 
 @Module({
-  imports: [OrganazationModule, AuthModule, PrismaModule],
+  imports: [OrganazationModule, AuthModule],
 })
 export class AppModule { }
--- server/src/modules/administrative/organazation/organazation.module.ts
+++ server/src/modules/administrative/organazation/organazation.module.ts
@@ -2,15 +2,18 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-07-12 14:34:56
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-07-12 15:24:11
+ * @LastEditTime: 2024-07-12 16:09:58
  * @Description: OrganazationModule
  */
 import { Module } from '@nestjs/common';
+
+import { PrismaModule } from '@/modules/prisma/prisma.module';
 
 import { OrganazationController } from './organazation.controller';
 import { OrganazationService } from './organazation.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [OrganazationController],
   providers: [OrganazationService],
   exports: [OrganazationService],
