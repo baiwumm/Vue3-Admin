814aabdffbe6fc649e054ed241193521791598e9
baiwumm <baiwumm@foxmail.com>
2024-07-12 15:55:49 +0800

    feat: 配置本地开发代理环境

--- web/src/service/request/index.ts
+++ web/src/service/request/index.ts
@@ -21,7 +21,7 @@ export const request = createFlatRequest<App.Service.Response, RequestInstanceSt
     async onRequest(config) {
       const { headers } = config;
 
-      // set token
+      // 添加 token 到请求头
       const token = localStg.get('token');
       const Authorization = token ? `Bearer ${token}` : null;
       Object.assign(headers, { Authorization });
@@ -29,8 +29,8 @@ export const request = createFlatRequest<App.Service.Response, RequestInstanceSt
       return config;
     },
     isBackendSuccess(response) {
-      // when the backend response code is "0000"(default), it means the request is success
-      // to change this logic by yourself, you can modify the `VITE_SERVICE_SUCCESS_CODE` in `.env` file
+      // 当后端返回的 code 为 200 (默认) 时，表示请求成功
+      // 如果需要修改这个逻辑，可以在 `.env` 文件中修改 `VITE_SERVICE_SUCCESS_CODE`
       return String(response.data.code) === import.meta.env.VITE_SERVICE_SUCCESS_CODE;
     },
     async onBackendFail(response, instance) {
@@ -47,19 +47,19 @@ export const request = createFlatRequest<App.Service.Response, RequestInstanceSt
         request.state.errMsgStack = request.state.errMsgStack.filter(msg => msg !== response.data.msg);
       }
 
-      // when the backend response code is in `logoutCodes`, it means the user will be logged out and redirected to login page
+      // 当后端返回的 code 在 `logoutCodes` 中时，表示用户需要退出登录
       const logoutCodes = import.meta.env.VITE_SERVICE_LOGOUT_CODES?.split(',') || [];
-      if (logoutCodes.includes(response.data.code)) {
+      if (logoutCodes.includes(String(response.data.code))) {
         handleLogout();
         return null;
       }
 
-      // when the backend response code is in `modalLogoutCodes`, it means the user will be logged out by displaying a modal
+      // 当后端返回的 code 在 `modalLogoutCodes` 中时，表示用户需要退出登录，通过弹窗形式提醒
       const modalLogoutCodes = import.meta.env.VITE_SERVICE_MODAL_LOGOUT_CODES?.split(',') || [];
-      if (modalLogoutCodes.includes(response.data.code) && !request.state.errMsgStack?.includes(response.data.msg)) {
+      if (modalLogoutCodes.includes(String(response.data.code)) && !request.state.errMsgStack?.includes(response.data.msg)) {
         request.state.errMsgStack = [...(request.state.errMsgStack || []), response.data.msg];
 
-        // prevent the user from refreshing the page
+        // 防止用户刷新页面
         window.addEventListener('beforeunload', handleLogout);
 
         window.$modal?.error({
@@ -78,10 +78,10 @@ export const request = createFlatRequest<App.Service.Response, RequestInstanceSt
         return null;
       }
 
-      // when the backend response code is in `expiredTokenCodes`, it means the token is expired, and refresh token
-      // the api `refreshToken` can not return error code in `expiredTokenCodes`, otherwise it will be a dead loop, should return `logoutCodes` or `modalLogoutCodes`
+      // 当后端返回的 code 在 `expiredTokenCodes` 中时，表示 token 过期，需要刷新 token
+      // `refreshToken` 接口不能返回 `expiredTokenCodes` 中的错误码，否则会死循环，应该返回 `logoutCodes` 或 `modalLogoutCodes`
       const expiredTokenCodes = import.meta.env.VITE_SERVICE_EXPIRED_TOKEN_CODES?.split(',') || [];
-      if (expiredTokenCodes.includes(response.data.code) && !request.state.isRefreshingToken) {
+      if (expiredTokenCodes.includes(String(response.data.code)) && !request.state.isRefreshingToken) {
         request.state.isRefreshingToken = true;
 
         const refreshConfig = await handleRefreshToken(response.config);
@@ -99,29 +99,21 @@ export const request = createFlatRequest<App.Service.Response, RequestInstanceSt
       return response.data.data;
     },
     onError(error) {
-      // when the request is fail, you can show error message
+      // 当请求失败时，可以在这里处理显示错误信息的逻辑
+      let message = error.response?.data?.msg || error.message;
+      let backendErrorCode = error.response?.data?.code;
 
-      let message = error.message;
-      let backendErrorCode = '';
-
-      // get backend error message and code
-      if (error.code === BACKEND_ERROR_CODE) {
-        message = error.response?.data?.msg || message;
-        backendErrorCode = error.response?.data?.code || '';
-      }
-
-      // the error message is displayed in the modal
+      // 错误信息通过弹窗形式显示
       const modalLogoutCodes = import.meta.env.VITE_SERVICE_MODAL_LOGOUT_CODES?.split(',') || [];
-      if (modalLogoutCodes.includes(backendErrorCode)) {
+      if (modalLogoutCodes.includes(String(backendErrorCode))) {
         return;
       }
 
-      // when the token is expired, refresh token and retry request, so no need to show error message
+      // 当 token 过期时，刷新 token 并重试请求，所以不需要显示错误信息
       const expiredTokenCodes = import.meta.env.VITE_SERVICE_EXPIRED_TOKEN_CODES?.split(',') || [];
-      if (expiredTokenCodes.includes(backendErrorCode)) {
+      if (expiredTokenCodes.includes(String(backendErrorCode))) {
         return;
       }
-
       showErrorMsg(request.state, message);
     }
   }
@@ -164,7 +156,7 @@ export const demoRequest = createRequest<App.Service.DemoResponse>(
         message = error.response?.data?.message || message;
       }
 
-      window.$message?.error(message);
+      window.$message?.error(message + 111);
     }
   }
 );
