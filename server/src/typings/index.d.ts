e183fce697c6dcb2ee7e7231aa42273e52e0f8e2
baiwumm <baiwumm@foxmail.com>
2024-07-15 11:16:17 +0800

    perf: 修改树形数据不分页的 type 类型提示

--- server/src/typings/index.d.ts
+++ server/src/typings/index.d.ts
@@ -13,9 +13,9 @@ declare namespace Api {
      * @description: 分页数据
      */
     type PageResponse<T = any> = {
-      current: number; // 页码
-      size: number; // 当前页条数
-      total: number; // 总条数
+      current?: number; // 页码
+      size?: number; // 当前页条数
+      total?: number; // 总条数
       records: T[]; // 业务数据
     };
     // 公共列
