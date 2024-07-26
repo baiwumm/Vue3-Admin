e6151ffd4fc834c8f212491848a2fd262ba5768d
baiwumm <baiwumm@foxmail.com>
2024-07-26 17:48:36 +0800

    feat(components): 新增 figure-labels 组件

---
+++ web/src/components/custom/figure-labels.vue
@@ -0,0 +1,73 @@
+<template>
+  <template v-for="tag in value" :key="tag">
+    <a-tooltip v-if="tag.length > 20" :title="tag">
+      <a-tag closable @close="handleClose(tag)">
+        {{ `${tag.slice(0, 20)}...` }}
+      </a-tag>
+    </a-tooltip>
+    <a-tag v-else closable @close="handleClose(tag)">
+      {{ tag }}
+    </a-tag>
+  </template>
+  <a-input
+    v-if="state.inputVisible"
+    ref="inputRef"
+    v-model:value="state.inputValue"
+    type="text"
+    size="small"
+    :style="{ width: '78px' }"
+    @blur="handleInputConfirm"
+    @keyup.enter="handleInputConfirm"
+  />
+  <a-tag
+    v-else
+    style="background: #fff; border-style: dashed"
+    @click="showInput"
+  >
+    <plus-outlined />
+    New Tag
+  </a-tag>
+</template>
+<script lang="ts" setup>
+import { ref, reactive, nextTick } from "vue";
+import { PlusOutlined } from "@ant-design/icons-vue";
+
+// 父组件传递的值
+type Props = {
+  value: string[];
+};
+const props = defineProps<Props>();
+
+const emit = defineEmits(["update:value"]);
+
+const inputRef = ref();
+const state = reactive({
+  inputVisible: false,
+  inputValue: "",
+});
+
+const handleClose = (removedTag: string) => {
+  const tags = props.value.filter((tag: string) => tag !== removedTag);
+  emit("update:value", tags);
+};
+
+const showInput = () => {
+  state.inputVisible = true;
+  nextTick(() => {
+    inputRef.value.focus();
+  });
+};
+
+const handleInputConfirm = () => {
+  const inputValue = state.inputValue;
+  let tags = props.value;
+  if (inputValue && tags.indexOf(inputValue) === -1) {
+    tags = [...tags, inputValue];
+  }
+  Object.assign(state, {
+    inputVisible: false,
+    inputValue: "",
+  });
+  emit("update:value", tags);
+};
+</script>
