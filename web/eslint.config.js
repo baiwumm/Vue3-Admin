893de0b6d462721c501cdd582206f749f744a5f0
baiwumm <baiwumm@foxmail.com>
2024-07-26 17:48:04 +0800

    ci(package.json): 新增 eslint-plugin-simple-import-sort 包

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from '@soybeanjs/eslint-config';
+import { defineConfig } from "@soybeanjs/eslint-config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default defineConfig(
   { vue: true, unocss: true },
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
-      'vue/multi-word-component-names': [
-        'warn',
+      "vue/multi-word-component-names": [
+        "warn",
         {
-          ignores: ['index', 'App', 'Register', '[id]', '[url]']
-        }
+          ignores: ["index", "App", "Register", "[id]", "[url]"],
+        },
       ],
-      'vue/component-name-in-template-casing': [
-        'warn',
-        'PascalCase',
+      "vue/component-name-in-template-casing": [
+        "warn",
+        "PascalCase",
         {
           registeredComponentsOnly: false,
-          ignores: ['/^icon-/']
-        }
+          ignores: ["/^icon-/"],
+        },
       ],
-      'unocss/order-attributify': 'off'
-    }
-  }
+      "unocss/order-attributify": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 );
