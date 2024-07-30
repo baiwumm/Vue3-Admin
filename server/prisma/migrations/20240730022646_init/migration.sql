6a3c2d348810fc7f8ae85a1685a349c823b96e78
baiwumm <baiwumm@foxmail.com>
2024-07-30 15:09:52 +0800

    refactor(prisma): 修改 User 模型

--- server/prisma/migrations/20240730022646_init/migration.sql
+++ server/prisma/migrations/20240730022646_init/migration.sql
@@ -1,3 +1,9 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "Sex" AS ENUM ('FEMALE', 'MALE');
+
 -- CreateTable
 CREATE TABLE "Organization" (
     "id" TEXT NOT NULL,
@@ -27,6 +33,33 @@ CREATE TABLE "Post" (
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cnName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "sex" "Sex" NOT NULL DEFAULT 'FEMALE',
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "sort" INTEGER NOT NULL,
+    "token" TEXT,
+    "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "city" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "address" TEXT,
+    "orgId" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "loginCount" INTEGER NOT NULL DEFAULT 0,
+    "lastIp" TEXT,
+    "lastLoginAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Organization_name_key" ON "Organization"("name");
 
@@ -36,6 +69,15 @@ CREATE UNIQUE INDEX "Organization_code_key" ON "Organization"("code");
 -- CreateIndex
 CREATE UNIQUE INDEX "Post_name_key" ON "Post"("name");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
 -- AddForeignKey
 ALTER TABLE "Organization" ADD CONSTRAINT "Organization_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -44,3 +86,9 @@ ALTER TABLE "Post" ADD CONSTRAINT "Post_orgId_fkey" FOREIGN KEY ("orgId") REFERE
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
