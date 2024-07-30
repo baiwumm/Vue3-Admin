384ad9d92dfd4d92537f4e6ab67d17824d7df1f4
baiwumm <baiwumm@foxmail.com>
2024-07-30 15:14:18 +0800

    feat: 配置本地文件访问路径

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,14 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-07-09 15:26:41
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-07-17 15:51:32
+ * @LastEditTime: 2024-07-30 15:10:21
  * @Description: 全局入口文件
  */
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 import { AllExceptionsFilter } from '@/filter/all-exception.filter'; // 全局异常过滤器
 import { HttpExceptionsFilter } from '@/filter/http-exception.filter'; // http 异常过滤器
@@ -14,7 +17,7 @@ import { ValidationPipe } from '@/pipe/validation.pipe'; // 参数校验
 
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // 错误异常捕获 和 过滤处理
   app.useGlobalFilters(new AllExceptionsFilter());
@@ -22,6 +25,10 @@ async function bootstrap() {
 
   // 全局参数校验
   app.useGlobalPipes(new ValidationPipe());
+
+  // 配置文件访问  文件夹为静态目录，以达到可直接访问下面文件的目的
+  const rootDir = join(__dirname, '..');
+  app.use('/static', express.static(join(rootDir, '/upload')));
 
   // 构建swagger文档
   const options = new DocumentBuilder()
