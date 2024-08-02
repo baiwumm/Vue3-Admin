790b728a9f54cdccdc15af37ab809468197898a3
baiwumm <baiwumm@foxmail.com>
2024-08-02 10:06:00 +0800

    feat(filter): 修改全局过滤器提示

--- server/src/filter/all-exception.filter.ts
+++ server/src/filter/all-exception.filter.ts
@@ -2,27 +2,25 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-07-10 14:17:49
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-07-12 13:43:07
+ * @LastEditTime: 2024-08-02 09:41:51
  * @Description: 全局异常过滤器
  */
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
 
-import { RESPONSE_MSG } from '@/enums';
 import { responseMessage } from '@/utils';
 
 // @Catch() 装饰器绑定所需的元数据到异常过滤器上。它告诉 Nest这个特定的过滤器正在寻找
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost) {
     // 获取上下文
     const ctx = host.switchToHttp();
     // 获取响应体
     const response = ctx.getResponse<Response>();
     // 获取状态码，判断是HTTP异常还是服务器异常
     const statusCode = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
-
     // 自定义异常返回体
-    response.status(statusCode).json(responseMessage(null, RESPONSE_MSG.ERROR, statusCode));
+    response.status(statusCode).json(responseMessage(null, exception.message, statusCode));
   }
 }
