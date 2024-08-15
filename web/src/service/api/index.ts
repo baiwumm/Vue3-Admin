f60804d3ad4ac202ef70c8233c8e48a8c39c9305
baiwumm <baiwumm@foxmail.com>
2024-08-15 16:08:28 +0800

    feat(service): 新增国际化 CURD 接口

--- web/src/service/api/index.ts
+++ web/src/service/api/index.ts
@@ -3,4 +3,5 @@ export * from './route';
 export * from './administrative/organization';
 export * from './administrative/post-manage';
 export * from './system-manage/user-manage';
+export * from './system-manage/internalization';
 export * from './system-manage/operation-log';
---
+++ web/src/service/api/system-manage/internalization.ts
@@ -0,0 +1,52 @@
+/*
+ * @Author: 白雾茫茫丶<baiwumm.com>
+ * @Date: 2024-08-15 11:06:40
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2024-08-15 11:15:41
+ * @Description: 国际化模块接口
+ */
+import { request } from "@/service/request";
+
+const baseURL = '/system/internalization'
+
+/**
+ * @description: 获取国际化列表
+ * @param {Api.SystemManage.InternalizationSearchParams} params
+ */
+export const getInternalizationList = (params?: Api.SystemManage.InternalizationSearchParams) =>
+  request<Api.Common.PaginatingQueryRecord<Api.SystemManage.Internalization>>({
+    url: baseURL,
+    params,
+  });
+
+/**
+* @description: 创建国际化
+* @param {Api.SystemManage.SaveInternalization} body
+*/
+export const createInternalization = (body: Api.SystemManage.SaveInternalization) =>
+  request<Api.SystemManage.Internalization>({
+    url: baseURL,
+    method: "post",
+    data: body,
+  });
+
+/**
+* @description: 更新国际化
+* @param {Api.SystemManage.SaveInternalization} body
+*/
+export const updateInternalization = ({ id, ...body }: Api.SystemManage.SaveInternalization) =>
+  request<Api.SystemManage.Internalization>({
+    url: `${baseURL}/${id}`,
+    method: "patch",
+    data: body,
+  });
+
+/**
+* @description: 删除国际化
+* @param {Api.Common.ColumnId} body
+*/
+export const delInternalization = ({ id }: Api.Common.ColumnId) =>
+  request<Api.SystemManage.Internalization>({
+    url: `${baseURL}/${id}`,
+    method: "delete",
+  });
