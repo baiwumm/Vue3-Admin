0d3e40ae6472133d3806a9d858906d3cb9d39e30
baiwumm <baiwumm@foxmail.com>
2024-08-29 15:40:11 +0800

    style(enum): 格式化代码风格

--- web/src/enum/index.ts
+++ web/src/enum/index.ts
@@ -1,50 +1,37 @@
-
-/**
- * @description: 仓库状态 id
- */
+/** @description: 仓库状态 id */
 export enum SetupStoreId {
   App = 'app-store',
   Theme = 'theme-store',
   Auth = 'auth-store',
   Route = 'route-store',
-  Tab = 'tab-store'
+  Tab = 'tab-store',
 }
 
-/**
- * @description: 操作类型
- */
+/** @description: 操作类型 */
 export enum OPERATION_TYPE {
   ADD = 'add',
   EDIT = 'edit',
   DELETE = 'delete',
 }
 
-/**
- * @description: 公共文案
- */
+/** @description: 公共文案 */
 export enum UNIFORM_TEXT {
   NULL = '--',
 }
 
-/**
- * @description: 状态
- */
+/** @description: 状态 */
 export enum STATUS {
   ACTIVE = 'ACTIVE',
   INACTIVE = 'INACTIVE',
 }
 
-/**
- * @description: 用户性别
- */
+/** @description: 用户性别 */
 export enum SEX {
   FEMALE = 'FEMALE',
   MALE = 'MALE',
 }
 
-/**
- * @description: 请求方法
- */
+/** @description: 请求方法 */
 export enum METHOD {
   GET = 'GET',
   POST = 'POST',
@@ -53,18 +40,14 @@ export enum METHOD {
   DELETE = 'DELETE',
 }
 
-/**
- * @description: 菜单类型
- */
+/** @description: 菜单类型 */
 export enum MENU_TYPE {
   DIRECTORY = 'DIRECTORY', // 目录
   MENU = 'MENU', // 菜单
   BUTTON = 'BUTTON', // 按钮
 }
 
-/**
- * @description: 个人中心 - 安全设置
- */
+/** @description: 个人中心 - 安全设置 */
 export enum PERSONAL_SETTING {
   BASIC_SETTING = 'basicSetting', // 基本设置
   SECURITY_SETTING = 'securitySetting', // 安全设置
