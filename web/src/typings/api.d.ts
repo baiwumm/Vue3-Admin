6c0d805960fd794773eb415a62abf53be15019bd
baiwumm <baiwumm@foxmail.com>
2024-08-29 10:51:55 +0800

    feat(typing): 格式化代码风格

--- web/src/typings/api.d.ts
+++ web/src/typings/api.d.ts
@@ -1,40 +1,30 @@
 declare namespace Api {
   namespace Common {
-    /**
-    * @description: 请求分页参数
-    */
+    /** @description: 请求分页参数 */
     type PaginatingParams = {
       current: number; // 页码
       size: number; // 条数
     };
-    /**
-     * @description: 分页响应体
-     */
+    /** @description: 分页响应体 */
     type PaginatingResponse = {
-      total: number; //总条数
+      total: number; // 总条数
     } & PaginatingParams;
 
-    /**
-     * @description: 分页列表
-     */
+    /** @description: 分页列表 */
     type PaginatingQueryRecord<T = any> = {
       records: T[];
     } & PaginatingResponse;
 
-    /**
-     * @description: 查询时间
-     */
+    /** @description: 查询时间 */
     type SearchTime = {
       startTime?: number; // 开始时间
       endTime?: number; // 结束时间
-    }
+    };
 
-    /**
-     * @description: 公共字段
-     */
+    /** @description: 公共字段 */
     type ColumnId = {
       id: string; // 主键
-    }
+    };
     type ColumnFields = {
       createdAt: string; // 创建时间
       updatedAt?: string; // 更新时间
@@ -43,41 +33,31 @@ declare namespace Api {
     type CommonRecord<T = any> = ColumnFields & T;
   }
 
-  /**
-   * @description: 身份鉴权
-   */
+  /** @description: 身份鉴权 */
   namespace Auth {
-    /**
-     * @description: 登录 token
-     */
+    /** @description: 登录 token */
     type LoginToken = {
       token: string;
-    }
+    };
 
-    /**
-     * @description: 登录用户信息
-     */
+    /** @description: 登录用户信息 */
     type UserInfo = Omit<SystemManage.UserManage, 'password' | 'token'> & {
       buttons: string[]; // 按钮权限
       roles: string[]; // 角色编码
-    }
-    /**
-     * @description: 登录参数
-     */
+    };
+    /** @description: 登录参数 */
     type LoginParams = {
       userName: string; // 用户名
       password: string; // 密码
       captchaCode: string; // 验证码
-    }
+    };
 
-    /**
-     * @description: 获取掘金文章参数
-     */
+    /** @description: 获取掘金文章参数 */
     type JuejinParams = {
       sort_type: number;
       cursor: string;
       user_id: string;
-    }
+    };
   }
 
   /**
@@ -98,13 +78,9 @@ declare namespace Api {
     }
   }
 
-  /**
-   * @description: 系统管理
-   */
+  /** @description: 系统管理 */
   namespace SystemManage {
-    /**
-     * @description: 用户管理
-     */
+    /** @description: 用户管理 */
     type UserManage = Common.CommonRecord<{
       userName: string; // 用户名
       password?: string; // 密码
@@ -128,38 +104,40 @@ declare namespace Api {
       loginCount: number; // 登录次数
       lastIp?: string; // 最后一次登录 ip
       lastLoginAt?: string; // 最后登录时间
-    }>
-    /**
-     * @description: 查询参数
-     */
+    }>;
+    /** @description: 查询参数 */
     type UserManageSearchParams = Partial<Pick<UserManage, 'userName' | 'status'>> & Api.Common.PaginatingParams;
-    /**
-     * @description: 创建/更新用户
-     */
-    type SaveUserManage = Pick<UserManage, 'userName' | 'cnName' | 'phone' | 'email' | 'sex' | 'status' | 'sort' | 'city' | 'address' | 'tags' | 'avatar' | 'password'>
-      & Partial<Pick<UserManage, 'roleId' | 'orgId' | 'postId'>> & Partial<Api.Common.ColumnId> & {
+    /** @description: 创建/更新用户 */
+    type SaveUserManage = Pick<
+      UserManage,
+      | 'userName'
+      | 'cnName'
+      | 'phone'
+      | 'email'
+      | 'sex'
+      | 'status'
+      | 'sort'
+      | 'city'
+      | 'address'
+      | 'tags'
+      | 'avatar'
+      | 'password'
+    > &
+      Partial<Pick<UserManage, 'roleId' | 'orgId' | 'postId'>> &
+      Partial<Api.Common.ColumnId> & {
         confirmPassword?: string;
       };
-    /**
-     * @description: 修改用户密码
-     */
+    /** @description: 修改用户密码 */
     type ChangeUserPassword = {
       password: string; // 原密码
       newPassword: string; // 新密码
-    }
-    /**
-     * @description: 修改密码
-     */
+    };
+    /** @description: 修改密码 */
     type EditPassword = {
       oldPassword: string; // 原密码
-    } & Required<Pick<
-      SaveUserManage,
-      "password" | "confirmPassword"
-    >>
+    } & Required<Pick<SaveUserManage, 'password' | 'confirmPassword'>>;
 
-    /**
-     * @description: 菜单管理
-     */
+    /** @description: 菜单管理 */
     type MenuManage = Common.CommonRecord<{
       title: string; // 菜单名称
       type: import('@/enum').MENU_TYPE; // 菜单类型
@@ -171,40 +149,31 @@ declare namespace Api {
       permission?: string; // 按钮权限
       sort: number; // 排序
       children?: MenuManage[];
-    }>
-    /**
-     * @description: 查询参数
-     */
+    }>;
+    /** @description: 查询参数 */
     type MenuManageSearchParams = Partial<Pick<MenuManage, 'title' | 'name'>> & Api.Common.SearchTime;
-    /**
-     * @description: 创建/更新用户
-     */
+    /** @description: 创建/更新用户 */
     type SaveMenuManage = Omit<MenuManage, keyof Api.Common.ColumnFields> & Partial<Api.Common.ColumnId>;
 
-    /**
-     * @description: 角色管理
-     */
+    /** @description: 角色管理 */
     type RoleManage = Common.CommonRecord<{
       name: string; // 角色名称
       code: string; // 角色编码
       description?: string; // 角色描述
       sort: number; // 排序
       permissions: string[]; // 权限
-    }>
-    /**
-     * @description: 查询参数
-     */
-    type RoleManageSearchParams = Partial<Pick<RoleManage, 'name' | 'code'>> & Api.Common.SearchTime & Api.Common.PaginatingParams;
-    /**
-     * @description: 创建/更新角色
-     */
-    type SaveRoleManage = Omit<RoleManage, keyof Api.Common.ColumnFields | 'permissions'> & Partial<Api.Common.ColumnId> & {
-      menus: string[];
-    };
+    }>;
+    /** @description: 查询参数 */
+    type RoleManageSearchParams = Partial<Pick<RoleManage, 'name' | 'code'>> &
+      Api.Common.SearchTime &
+      Api.Common.PaginatingParams;
+    /** @description: 创建/更新角色 */
+    type SaveRoleManage = Omit<RoleManage, keyof Api.Common.ColumnFields | 'permissions'> &
+      Partial<Api.Common.ColumnId> & {
+        menus: string[];
+      };
 
-    /**
-     * @description: 国际化
-     */
+    /** @description: 国际化 */
     type Internalization = Common.CommonRecord<{
       name: string; // 国际化字段
       parentId?: string; // 父级id
@@ -213,19 +182,13 @@ declare namespace Api {
       jaJP?: string; // 日文
       zhTW?: string; // 繁体中文
       children?: Internalization[];
-    }>
-    /**
-     * @description: 查询参数
-     */
+    }>;
+    /** @description: 查询参数 */
     type InternalizationSearchParams = Partial<Pick<Internalization, 'name'>> & Api.Common.SearchTime;
-    /**
-     * @description: 创建/更新国际化
-     */
-    type SaveInternalization = Omit<Internalization, keyof Api.Common.ColumnFields> & Partial<Api.Common.ColumnId>
+    /** @description: 创建/更新国际化 */
+    type SaveInternalization = Omit<Internalization, keyof Api.Common.ColumnFields> & Partial<Api.Common.ColumnId>;
 
-    /**
-     * @description: 操作日志
-     */
+    /** @description: 操作日志 */
     type OperationLog = Common.CommonRecord<{
       userId: string; // 关联用户 id
       user: UserManage;
@@ -235,46 +198,34 @@ declare namespace Api {
       params: Record<string, any>; // 请求参数
       os: string; // 操作系统
       browser: string; // 浏览器
-    }>
-    /**
-     * @description: 查询参数
-     */
-    type OperationLogSearchParams = Partial<Pick<OperationLog, 'userId' | 'method'>> & Api.Common.SearchTime & Api.Common.PaginatingParams;
-    /**
-     * @description: 删除参数
-     */
-    type OperationLogDelParams = { ids: string[] }
+    }>;
+    /** @description: 查询参数 */
+    type OperationLogSearchParams = Partial<Pick<OperationLog, 'userId' | 'method'>> &
+      Api.Common.SearchTime &
+      Api.Common.PaginatingParams;
+    /** @description: 删除参数 */
+    type OperationLogDelParams = { ids: string[] };
   }
 
-  /**
-   * @description: 智能行政
-   */
+  /** @description: 智能行政 */
   namespace Administrative {
-    /**
-     * @description: 组织管理
-     */
+    /** @description: 组织管理 */
     type Organization = Common.CommonRecord<{
       name: string; // 组织名称
       code: string; // 组织编码
-      parentId?: string; // 父级 id
+      parentId: string | null; // 父级 id
       description?: string; // 组织描述
       icon?: string; // 组织图标
       sort: number; // 排序
       posts?: PostManage[]; // 关联岗位
-      children?: Organization[]
-    }>
-    /**
-     * @description: 查询参数
-     */
+      children?: Organization[];
+    }>;
+    /** @description: 查询参数 */
     type OrganizationSearchParams = Partial<Pick<Organization, 'name' | 'code'>>;
-    /**
-     * @description: 创建/更新组织
-     */
+    /** @description: 创建/更新组织 */
     type SaveOrganization = Omit<Organization, keyof Api.Common.ColumnFields | 'posts'> & Partial<Api.Common.ColumnId>;
 
-    /**
-     * @description: 岗位管理
-     */
+    /** @description: 岗位管理 */
     type PostManage = Common.CommonRecord<{
       name: string; // 岗位名称
       parentId?: string; // 父级 id
@@ -282,15 +233,12 @@ declare namespace Api {
       organization: Organization; // 所属组织
       sort: number; // 排序
       description?: string; // 岗位描述
-      children?: PostManage[]
-    }>
-    /**
-     * @description: 查询参数
-     */
+      children?: PostManage[];
+    }>;
+    /** @description: 查询参数 */
     type PostManageSearchParams = Partial<Pick<PostManage, 'name' | 'orgId'>> & Api.Common.SearchTime;
-    /**
-     * @description: 创建/更新岗位
-     */
-    type SavePostManage = Omit<PostManage, keyof Api.Common.ColumnFields | 'organization'> & Partial<Api.Common.ColumnId>;
+    /** @description: 创建/更新岗位 */
+    type SavePostManage = Omit<PostManage, keyof Api.Common.ColumnFields | 'organization'> &
+      Partial<Api.Common.ColumnId>;
   }
 }
