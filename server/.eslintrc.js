9c6497da57248d9ca10eae544904cf049e8948e9
baiwumm <baiwumm@foxmail.com>
2024-09-02 11:16:37 +0800

    perf(eslint): 修改 eslint 规则

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 };
