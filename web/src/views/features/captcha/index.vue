4396bb71f5f56f6297ee3f035e9bdc24a8e95780
baiwumm <baiwumm@foxmail.com>
2024-09-11 15:40:23 +0800

    feat(captcha): 新增 PuzzleCode 拼图验证码

--- web/src/views/features/captcha/index.vue
+++ web/src/views/features/captcha/index.vue
@@ -3,6 +3,7 @@ import { $t } from '@/locales';
 
 import GraphicCode from './modules/graphic-code.vue'; // 图形验证码
 import OperationCode from './modules/operation-code.vue'; // 运算验证码
+import PuzzleCode from './modules/puzzle-code.vue'; // 拼图验证码
 
 // 国际化
 const locales = (field: string) => $t(`page.features.captcha.${field}`);
@@ -13,7 +14,7 @@ const locales = (field: string) => $t(`page.features.captcha.${field}`);
     <ACard :bordered="false" :body-style="{ padding: 0 }">
       <APageHeader :title="$t('route.features_captcha')">{{ locales('subTitle') }}</APageHeader>
     </ACard>
-    <ARow :gutter="20">
+    <ARow :gutter="[20, 20]">
       <!-- 图形验证码 -->
       <ACol :xs="24" :sm="12" :md="12" :lg="12" :xl="8">
         <ACard :bordered="false" :title="locales('graphicCode')">
@@ -24,6 +25,12 @@ const locales = (field: string) => $t(`page.features.captcha.${field}`);
       <ACol :xs="24" :sm="12" :md="12" :lg="12" :xl="8">
         <ACard :bordered="false" :title="locales('operationCode')">
           <OperationCode :locales="locales" />
+        </ACard>
+      </ACol>
+      <!-- 拼图验证码 -->
+      <ACol :xs="24" :sm="12" :md="12" :lg="12" :xl="8">
+        <ACard :bordered="false" :title="locales('puzzleCode')">
+          <PuzzleCode :locales="locales" />
         </ACard>
       </ACol>
     </ARow>
---
+++ web/src/views/features/captcha/modules/puzzle-code.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Vcode from 'vue3-puzzle-vcode';
+
+defineOptions({
+  name: 'PuzzleCode',
+});
+
+// 父组件传递的值
+type Props = {
+  locales: (field: string) => string;
+};
+const props = defineProps<Props>();
+
+// 生成图片数组
+const imgs = Object.keys(import.meta.glob('@/assets/img/*.jpg'));
+
+/** @description: 验证通过回调 */
+const onSuccess = () => {
+  window.$message?.success(props.locales('verifySuccess'));
+};
+</script>
+
+<template>
+  <div class="puzzle-vcode-container">
+    <Vcode show type="inside" :imgs="imgs" class-name="puzzle-vcode" @success="onSuccess" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.puzzle-vcode-container {
+  :deep(.puzzle-vcode) {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
