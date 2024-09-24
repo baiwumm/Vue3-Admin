5ea54e9dda12b85c98dfe99e57dd597d6a31bcba
baiwumm <baiwumm@foxmail.com>
2024-09-24 16:21:20 +0800

    perf(theme): 修改默认主题色

--- web/src/theme/settings.ts
+++ web/src/theme/settings.ts
@@ -3,34 +3,34 @@ export const themeSettings: App.Theme.ThemeSetting = {
   themeScheme: 'light',
   grayscale: false,
   recommendColor: false,
-  themeColor: '#646cff',
+  themeColor: '#22C55E',
   otherColor: {
     info: '#2080f0',
     success: '#52c41a',
     warning: '#faad14',
-    error: '#f5222d'
+    error: '#f5222d',
   },
   isInfoFollowPrimary: true,
   layout: {
     mode: 'vertical',
-    scrollMode: 'content'
+    scrollMode: 'content',
   },
   page: {
     animate: true,
-    animateMode: 'fade-slide'
+    animateMode: 'fade-slide',
   },
   header: {
     height: 56,
     breadcrumb: {
       visible: true,
-      showIcon: true
-    }
+      showIcon: true,
+    },
   },
   tab: {
     visible: true,
     cache: true,
     height: 44,
-    mode: 'chrome'
+    mode: 'chrome',
   },
   fixedHeaderAndTab: true,
   sider: {
@@ -39,14 +39,14 @@ export const themeSettings: App.Theme.ThemeSetting = {
     collapsedWidth: 64,
     mixWidth: 90,
     mixCollapsedWidth: 64,
-    mixChildMenuWidth: 200
+    mixChildMenuWidth: 200,
   },
   footer: {
     visible: true,
     fixed: false,
     height: 60,
-    right: true
-  }
+    right: true,
+  },
 };
 
 /**
