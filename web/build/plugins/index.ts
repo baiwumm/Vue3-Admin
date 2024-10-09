75a13f9f4454257e22a09ffd547409da21591dc5
baiwumm <baiwumm@foxmail.com>
2024-10-09 16:22:43 +0800

    build: 修改打包配置

--- web/build/plugins/index.ts
+++ web/build/plugins/index.ts
@@ -1,19 +1,21 @@
-import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import VueDevtools from 'vite-plugin-vue-devtools';
+import copy from 'rollup-plugin-copy';
+import type { PluginOption } from 'vite';
 import progress from 'vite-plugin-progress';
+import VueDevtools from 'vite-plugin-vue-devtools';
+
+import { setupHtmlPlugin } from './html';
 import { setupElegantRouter } from './router';
 import { setupUnocss } from './unocss';
 import { setupUnplugin } from './unplugin';
-import { setupHtmlPlugin } from './html';
 
 export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
   const plugins: PluginOption = [
     vue({
       script: {
-        defineModel: true
-      }
+        defineModel: true,
+      },
     }),
     vueJsx(),
     VueDevtools(),
@@ -21,7 +23,10 @@ export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
     setupUnocss(viteEnv),
     ...setupUnplugin(viteEnv),
     progress(),
-    setupHtmlPlugin(buildTime)
+    setupHtmlPlugin(buildTime),
+    copy({
+      targets: [{ src: 'src/assets/office/*', dest: 'dist/assets/office' }],
+    }),
   ];
 
   return plugins;
