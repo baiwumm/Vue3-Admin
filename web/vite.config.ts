4d675b8ae1bafa7502b66885379c643a00eaf351
baiwumm <baiwumm@foxmail.com>
2024-10-09 13:53:47 +0800

    build: 打包分包配置

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,10 @@
 import process from 'node:process';
 import { URL, fileURLToPath } from 'node:url';
+
 import { defineConfig, loadEnv } from 'vite';
-import { setupVitePlugins } from './build/plugins';
+
 import { createViteProxy, getBuildTime } from './build/config';
+import { setupVitePlugins } from './build/plugins';
 
 export default defineConfig(configEnv => {
   const viteEnv = loadEnv(configEnv.mode, process.cwd()) as unknown as Env.ImportMeta;
@@ -14,19 +16,19 @@ export default defineConfig(configEnv => {
     resolve: {
       alias: {
         '~': fileURLToPath(new URL('./', import.meta.url)),
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use "./src/styles/scss/global.scss" as *;`
-        }
-      }
+          additionalData: `@use "./src/styles/scss/global.scss" as *;`,
+        },
+      },
     },
     plugins: setupVitePlugins(viteEnv, buildTime),
     define: {
-      BUILD_TIME: JSON.stringify(buildTime)
+      BUILD_TIME: JSON.stringify(buildTime),
     },
     server: {
       host: '0.0.0.0',
@@ -34,18 +36,27 @@ export default defineConfig(configEnv => {
       open: true,
       proxy: createViteProxy(viteEnv, configEnv.command === 'serve'),
       fs: {
-        cachedChecks: false
-      }
+        cachedChecks: false,
+      },
     },
     preview: {
-      port: 9725
+      port: 9725,
     },
     build: {
       reportCompressedSize: false,
       sourcemap: viteEnv.VITE_SOURCE_MAP === 'Y',
       commonjsOptions: {
-        ignoreTryCatch: false
-      }
-    }
+        ignoreTryCatch: false,
+      },
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
   };
 });
