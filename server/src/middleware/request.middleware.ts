aa7282a388617217f9e7f378e46eeabd6f668833
baiwumm <baiwumm@foxmail.com>
2024-10-09 10:45:22 +0800

    feat(requestMiddleware): 添加接口白名单

--- server/src/middleware/request.middleware.ts
+++ server/src/middleware/request.middleware.ts
@@ -2,7 +2,7 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-09-29 17:55:42
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-09-29 17:57:24
+ * @LastEditTime: 2024-10-09 10:19:48
  * @Description: 全局请求拦截中间件
  */
 import { NextFunction, Request, Response } from 'express';
@@ -13,6 +13,7 @@ export function requestMiddleware(req: Request, res: Response, next: NextFunctio
     req.method === 'GET' ||
     req.url.includes('/auth/login') ||
     req.url.includes('/auth/logout') ||
+    req.url.includes('/auth/juejin') ||
     req.url.includes('/common/')
   ) {
     next();
