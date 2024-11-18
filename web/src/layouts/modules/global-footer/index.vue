895716eb1b120de7a933c0b19b44b964d08bcba7
baiwumm <baiwumm@foxmail.com>
2024-11-18 14:42:54 +0800

    perf(layout): 修改底部版权信息

--- web/src/layouts/modules/global-footer/index.vue
+++ web/src/layouts/modules/global-footer/index.vue
@@ -8,7 +8,7 @@ defineOptions({
   <DarkModeContainer class="h-full flex-center">
     <ASpace direction="vertical" align="center" :size="0">
       <ASpace>
-        <a href="https://github.com/baiwumm/umi-admin" target="_blank" rel="noopener noreferrer">Umi Admin</a>
+        <a href="https://github.com/baiwumm/react-admin" target="_blank" rel="noopener noreferrer">React Admin</a>
         <a href="https://github.com/baiwumm/vue3-admin" target="_blank" rel="noopener noreferrer">
           <SvgIcon icon="ri:github-fill" class="text-base" />
         </a>
